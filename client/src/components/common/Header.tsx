3573aa0281704621a86bd45c5afc8ef79e7f3ca1
Anurag Ambekar <anuragambekar1997@gmail.com>
2024-03-04 20:59:24 +0530

    header chanes

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleSigninForm } from "../../redux/slice/appSlice";
 import { UserState } from "../../types/types";
 import { noUser } from "../../redux/slice/userSlice";
+// import axios from "axios";
 
 export const Header: React.FC = () => {
   const dispatch = useDispatch();
@@ -15,6 +16,16 @@ export const Header: React.FC = () => {
     localStorage.removeItem("token");
     dispatch(noUser());
   };
+  // const logout2 = async () => {
+  //   try {
+  //     await axios.get("http://localhost:8080/auth/logout");
+  //     // Once logout is successful, you may want to perform additional actions
+  //     console.log("Logout successful");
+  //   } catch (error) {
+  //     console.error("Error during logout:", error);
+  //   }
+  // };
+
   return (
     <div className="absolute top-0 flex justify-between w-full px-10 py-5 shadow-md">
       <div className="flex transition delay-100 hover:scale-110">
@@ -27,10 +38,20 @@ export const Header: React.FC = () => {
         </h1> */}
       </div>
       <div>
+        {/* <Button
+          variant="outlined"
+          className="px-3 py-2 text-sm font-medium transition delay-150 scale-110 border-black rounded-xl text-slate-900 w-28 hover:bg-black hover:text-white"
+          // onClick={() => (TOKEN ? logout() : dispatch(toggleSigninForm(true)))}
+          onClick={logout2}
+          sx={{ textTransform: "none" }}
+        >
+          {TOKEN ? "Logout" : "Login"}
+        </Button> */}
         <Button
           variant="outlined"
           className="px-3 py-2 text-sm font-medium transition delay-150 scale-110 border-black rounded-xl text-slate-900 w-28 hover:bg-black hover:text-white"
           onClick={() => (TOKEN ? logout() : dispatch(toggleSigninForm(true)))}
+          // onClick={logout2}
           sx={{ textTransform: "none" }}
         >
           {TOKEN ? "Logout" : "Login"}
